b3f48a7ca58dddd9c0f0f75a0b12e310a07831ab
Sadiq <mohammad@Sadiqs-MacBook-Air.local>
2026-06-29 15:45:33 -0700

    Find the top paying job

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,23 @@
+/*
+Find the top paying jobs
+    - Identify the top 10 jobs with highest salary which are Remote.
+    - Focus on job postings with specified salaries (remove nulls).
+    - Highlight he top-paying opportuinities for Data related jobs 
+*/
+
+
+SELECT 
+    Job_ds.job_id,
+    Job_ds.job_schedule_type,
+    Job_ds.job_title_short,
+    Job_ds.salary_year_avg,
+    Job_ds.job_location,
+    Job_ds.job_posted_date
+FROM 
+    job_postings_fact AS Job_ds
+WHERE 
+    Job_ds.job_location = 'Anywhere' AND 
+    Job_ds.salary_year_avg IS NOT NULL AND
+    Job_ds.job_title_short LIKE '%Data%'
+ORDER BY Job_ds.salary_year_avg DESC
+LIMIT 10;
