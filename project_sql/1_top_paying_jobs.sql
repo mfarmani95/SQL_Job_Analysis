aef4031110d75272ad74e6b3c8c15f567f1b4a0d
Sadiq <mohammad@Sadiqs-MacBook-Air.local>
2026-06-29 15:50:26 -0700

    Find the top paying job: Complete

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -8,16 +8,22 @@ Find the top paying jobs
 
 SELECT 
     Job_ds.job_id,
-    Job_ds.job_schedule_type,
     Job_ds.job_title_short,
+    company_dim.name AS company_name,
+    Job_ds.job_schedule_type,
     Job_ds.salary_year_avg,
     Job_ds.job_location,
     Job_ds.job_posted_date
+
 FROM 
     job_postings_fact AS Job_ds
+LEFT JOIN company_dim ON Job_ds.company_id = company_dim.company_id    
 WHERE 
     Job_ds.job_location = 'Anywhere' AND 
     Job_ds.salary_year_avg IS NOT NULL AND
     Job_ds.job_title_short LIKE '%Data%'
 ORDER BY Job_ds.salary_year_avg DESC
 LIMIT 10;
+
+-- SELECT * FROM company_dim LIMIT 2;
+-- SELECT * FROM job_postings_fact LIMIT 2;
